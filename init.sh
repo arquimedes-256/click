4bca5e7153437f96f16ec55c15eb3351706bdf21
arquimedes-256 <pablo.hernan.arg@gmail.com>
2016-07-12 17:35:31 -0300

    --no-edit

--- init.sh
+++ init.sh
@@ -5,5 +5,5 @@ sleep 1;
 echo '0'	> var/clickAdsReady.var
 echo "{}"	> var/WTFObject.json;
 cd /root/click;
-git pull origin master;
+git pull --no-edit origin master;
 nodejs main.js || bash init.sh
